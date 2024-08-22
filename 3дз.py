6e6dab08291cf9f7c97f3ba7f1888ae92033c264
Rafael59147 <Rafaelka1441@gmail.com>
2024-08-22 19:19:49 +0800

    Add files via upload

---
+++ 3дз.py
@@ -0,0 +1,5 @@
+one_name= int("12")
+two_name= float("1.5")
+three_name = "Python"
+four_name = (two_name / one_name)
+print('Курс:',three_name,', всего задач:',one_name,', затрачено часов:',two_name,', среднее время выполнения',four_name,'часа.' )
---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = "Илья"
+print(name, type(name))
+age = 27
+print(age, type(age))
+new_age = 30.5
+print(new_age, type(new_age))
+is_student = True
+print(is_student, type(is_student))
