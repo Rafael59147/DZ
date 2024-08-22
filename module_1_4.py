3c72a0e2141c0f87272a57adb54d66ef03f08bce
Rafael59147 <Rafaelka1441@gmail.com>
2024-08-22 20:12:11 +0800

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input('В каком городе вы проживаете? ')
+print(len(my_string))
+print('В каком городе вы проживаете?'.upper())
+print('В каком городе вы проживаете?'.lower())
+print('В каком городе вы проживаете?'.replace(' ', ''))
+print(my_string[:1:])
+print(my_string[-1::])
