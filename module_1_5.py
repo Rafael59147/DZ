976c9f10184c8dc96b7fba2243c610655fab6991
Rafael59147 <Rafaelka1441@gmail.com>
2024-08-28 21:35:44 +0800

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,12 @@
+immutable_var = 1, True, 1.5,'string'
+print(immutable_var)
+print(type(immutable_var))
+#immutable_var [1] = False
+#print(immutable_var)
+#кортеж, неизменяемый объект(это встроенная функция самой программы, которую не возможно изменить с помощью кода)
+mutable_list = ["one","two","three","four"]
+print(mutable_list)
+print(type(mutable_list))
+mutable_list = ["L","i","s","t"]
+print(mutable_list)
+print(type(mutable_list))
